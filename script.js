8631052966bf9c2441786c0e421413d9423147ef
Aminul islam <aminurrohman98@gmail.com>
2026-07-19 19:56:36 +0100

    Learned about constructor function

--- script.js
+++ script.js
@@ -19,3 +19,8 @@ matilda.calcAge();
 console.log(aminul.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(aminul));
 console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Home Care Agency';
+console.log(aminul.species);
+console.log(aminul.hasOwnProperty('firstName'));
+console.log(aminul.hasOwnProperty('species'));
