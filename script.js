8b6f094dfe51f39146bc1795dc584ad8c365d36c
Aminul islam <aminurrohman98@gmail.com>
2026-07-19 19:40:53 +0100

    Implement Person constructor and prototype methods; add VSCode settings for spell checking

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+// Construction function and the new operator
+const Person = function (firstName, birthYear) {
+  ((this.firstName = firstName), (this.birthYear = birthYear));
+};
+
+const aminul = new Person('Aminul islam', 2003);
+console.log(aminul);
+
+Person.prototype.calcAge = function () {
+  console.log(2026 - this.birthYear);
+};
+aminul.calcAge();
+
+// Different user
+const matilda = new Person('Matilda', 2000);
+matilda.calcAge();
+
+console.log(aminul.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(aminul));
+console.log(Person.prototype.isPrototypeOf(Person));
