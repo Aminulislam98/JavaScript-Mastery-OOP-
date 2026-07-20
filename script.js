3496115773d23bf6efc5603289b59c23450fce83
Aminul islam <aminurrohman98@gmail.com>
2026-07-20 12:57:29 +0100

    Add account object with getter and setter for latest movements

--- script.js
+++ script.js
@@ -68,3 +68,16 @@ islam.calcAge();
 //   console.log(`Hey ${this.firstName}`);
 // };
 islam.greet();
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+account.latest = 50;
+console.log(account.movements);
