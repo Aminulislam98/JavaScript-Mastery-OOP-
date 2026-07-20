2e5afd7e363716e9e8c60c0ae3a1425e1b956f17
Aminul islam <aminurrohman98@gmail.com>
2026-07-20 11:39:56 +0100

    Implement Car constructor and prototype methods for acceleration and braking

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ Person.prototype.species = 'Home Care Agency';
 console.log(aminul.species);
 console.log(aminul.hasOwnProperty('firstName'));
 console.log(aminul.hasOwnProperty('species'));
+
+// Challenge Exercise
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 120);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
